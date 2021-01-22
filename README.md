4502a49e5b9fb5148c7d2a0ab6e1c93c215963cb
ericgaspar <junk.eg@free.fr>
2021-01-22 11:37:00 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Navidrome for YunoHost
+
+[![Integration level](https://dash.yunohost.org/integration/navidrome.svg)](https://dash.yunohost.org/appci/app/navidrome) ![](https://ci-apps.yunohost.org/ci/badges/navidrome.status.svg) ![](https://ci-apps.yunohost.org/ci/badges/navidrome.maintain.svg)  
+[![Install Navidrome with YunoHost](https://install-app.yunohost.org/install-with-yunohost.svg)](https://install-app.yunohost.org/?app=navidrome)
+
+*[Lire ce readme en français.](./README_fr.md)*
+
+> *This package allows you to install Navidrome quickly and simply on a YunoHost server.  
+If you don't have YunoHost, please consult [the guide](https://yunohost.org/#/install) to learn how to install it.*
+
+## Overview
+Navidrome is an open source web-based music collection server and streamer. It gives you freedom to listen to your music collection from any browser or mobile device.
+
+**Shipped version:** 0.39.0
+
+## Screenshots
+
+![](https://raw.githubusercontent.com/deluan/navidrome/master/.github/screenshots/ss-desktop-player.png)
+
+## Demo
+
+* [Official demo](https://demo.navidrome.org/app/#/login)
+
+## Configuration
+
+#### Where are stored your music files
+
+Your music files are stored by default in your shared [multimedia folder](https://github.com/YunoHost-Apps/yunohost.multimedia) `/home/yunohost.multimedia/share/Music`. This folder is accessible from Nextcloud with *External Storages* enabled. This will allow you to easily upload your music files to the server.
+
+You can configure an alternative path to you music files by editing the path `MusicFolder = "/home/yunohost.multimedia/share/Music"` in this file `/var/lib/navidrome/navidrome.toml` using the [documentation](https://www.navidrome.org/docs/usage/configuration-options/).
+
+#### Client player
+
+You must activate *public site* if you want to connect a client player to Navidrome.
+
+## Documentation
+
+ * Official documentation: https://www.navidrome.org/docs/
+ * YunoHost documentation: https://yunohost.org/#/app_navidrome
+
+## YunoHost specific features
+
+#### Multi-user support
+
+* Is LDAP supported? **No**
+* Can the app be used by multiple users? **Yes**
+
+#### Supported architectures
+
+* x86-64 - [![Build Status](https://ci-apps.yunohost.org/ci/logs/navidrome%20%28Apps%29.svg)](https://ci-apps.yunohost.org/ci/apps/navidrome/)
+* ARMv8-A - [![Build Status](https://ci-apps-arm.yunohost.org/ci/logs/navidrome%20%28Apps%29.svg)](https://ci-apps-arm.yunohost.org/ci/apps/navidrome/)
+
+## Links
+
+ * Report a bug: https://github.com/YunoHost-Apps/navidrome_ynh/issues
+ * App website: https://www.navidrome.org/
+ * Upstream app repository: https://github.com/deluan/navidrome/
+ * YunoHost website: https://yunohost.org/
+
+---
+
+## Developer info
+
+Please send your pull request to the [testing branch](https://github.com/YunoHost-Apps/navidrome_ynh/tree/testing).
+
+To try the testing branch, please proceed like that.
+```
+sudo yunohost app install https://github.com/YunoHost-Apps/navidrome_ynh/tree/testing --debug
+or
+sudo yunohost app upgrade navidrome -u https://github.com/YunoHost-Apps/navidrome_ynh/tree/testing --debug
+```
---
+++ scripts/_common.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+pkg_dependencies="ffmpeg"
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
+
+# Check the architecture
+#
+# example: architecture=$(ynh_detect_arch)
+#
+# usage: ynh_detect_arch
+#
+# Requires YunoHost version 2.2.4 or higher.
+ynh_detect_arch(){
+        local architecture
+        if [ -n "$(uname -m | grep arm64)" ] || [ -n "$(uname -m | grep aarch64)" ]; then
+                architecture="arm64"
+        elif [ -n "$(uname -m | grep 86)" ]; then
+                architecture="i386"                
+        elif [ -n "$(uname -m | grep 64)" ]; then
+                architecture="x86-64"
+        elif [ -n "$(uname -m | grep armv7)" ]; then
+                architecture="armv7"
+        elif [ -n "$(uname -m | grep armv6)" ]; then
+                architecture="armv6"
+        elif [ -n "$(uname -m | grep armv5)" ]; then
+                architecture="armv5"
+        else
+                architecture="unknown"
+        fi
+        echo $architecture
+}
+
+
+# Install or update the main directory yunohost.multimedia
+#
+# usage: ynh_multimedia_build_main_dir
+ynh_multimedia_build_main_dir () {
+        local ynh_media_release="v1.2"
+        local checksum="806a827ba1902d6911095602a9221181"
+
+        # Download yunohost.multimedia scripts
+        wget -nv https://github.com/YunoHost-Apps/yunohost.multimedia/archive/${ynh_media_release}.tar.gz 2>&1
+
+        # Check the control sum
+        echo "${checksum} ${ynh_media_release}.tar.gz" | md5sum -c --status \
+                || ynh_die "Corrupt source"
+
+	# Check if the package acl is installed. Or install it.
+	ynh_package_is_installed 'acl' \
+		|| ynh_package_install acl
+
+        # Extract
+        mkdir yunohost.multimedia-master
+        tar -xf ${ynh_media_release}.tar.gz -C yunohost.multimedia-master --strip-components 1
+        ./yunohost.multimedia-master/script/ynh_media_build.sh
+}
+
+# Add a directory in yunohost.multimedia
+# This "directory" will be a symbolic link to a existing directory.
+#
+# usage: ynh_multimedia_addfolder "Source directory" "Destination directory"
+#
+# | arg: -s, --source_dir= - Source directory - The real directory which contains your medias.
+# | arg: -d, --dest_dir= - Destination directory - The name and the place of the symbolic link, relative to "/home/yunohost.multimedia"
+ynh_multimedia_addfolder () {
+	# Declare an array to define the options of this helper.
+	declare -Ar args_array=( [s]=source_dir= [d]=dest_dir= )
+	local source_dir
+	local dest_dir
+	# Manage arguments with getopts
+	ynh_handle_getopts_args "$@"
+
+	./yunohost.multimedia-master/script/ynh_media_addfolder.sh --source="$source_dir" --dest="$dest_dir"
+}
